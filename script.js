fff553ab4170545aede113c7cb76459ac1ee479d
sscefalix <100369942+sscefalix@users.noreply.github.com>
2023-04-16 15:48:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const embed = document.getElementById("embed")
+const embedFields = {}
+const code = {}
+const controllers = document.querySelectorAll(".controller")
+const color = document.getElementById("color")
+const showCodeBtn = document.getElementById("showCode")
+const modal = document.getElementById("jsonResponse")
+
+embed.querySelectorAll("div").forEach((element) => embedFields[element.className] = element)
+
+controllers.forEach((controller) => {
+    const input = controller.querySelector(".input")
+    if (input.getAttribute("type") === "color") return
+    const currentField = embedFields[input.id]
+
+    input.oninput = (event) => {
+        currentField.innerText = input.value
+
+        if(currentField.innerText.length > 0) {
+            currentField.style.display = "block"
+            code[input.id] = input.value
+        } else {
+            delete code[input.id]
+            currentField.style.display = "none"
+        }
+
+        if(input.value.length > input.getAttribute("maxlength")) input.value = input.value.slice(0, -1)
+        controller.setAttribute("data-length", input.value.length + '/' + input.getAttribute("maxlength"))
+    }
+})
+
+color.oninput = (event) => {
+    embed.style.borderColor = color.value
+}
+
+showCodeBtn.onclick = (event) => {
+    event.preventDefault()
+
+    modal.showModal()
+}
